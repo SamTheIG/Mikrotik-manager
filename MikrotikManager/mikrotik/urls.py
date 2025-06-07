acc559531052687a9bf70a9d6163399be4b3a2b3
Sam <miladian.samad@gmail.com>
2025-06-07 08:43:56 +0330

    routers connected to active user will be shown

--- MikrotikManager/mikrotik/urls.py
+++ MikrotikManager/mikrotik/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import MikIndex
 
 urlpatterns = [
-    path("", MikIndex, name="MikIndex")
+    path("", MikIndex, name="MikIndex"),
 ]
--- MikrotikManager/mikrotik/views.py
+++ MikrotikManager/mikrotik/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 from .models import Mikrotik
 
+
+@login_required
 def MikIndex(request):
-    Mikrotiks = Mikrotik.objects.all()
+    if request.user.is_authenticated:
+        user_id = request.user.id
+    Mikrotiks = Mikrotik.objects.filter(Userid = user_id)
     context = {
         "Mikrotiks": Mikrotiks
     }
