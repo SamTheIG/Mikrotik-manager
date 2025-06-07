8d458354486678d141a006e767e7b3e2ae4fbb81
Sam <miladian.samad@gmail.com>
2025-06-07 14:22:08 +0330

    commands are ready to use, i think!

--- MikrotikManager/mikrotik/urls.py
+++ MikrotikManager/mikrotik/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import MikIndex, MikManger
+from .views import MikIndex, MikManger, MikCommand
 
 urlpatterns = [
         path("", MikIndex, name="MikIndex"),
         path("<str:mikrotik_name>/", MikManger, name="MikManger"),
+        path("<str:mikrotik_name>/<str:Routercommand>/", MikCommand, name="MikCommand")
     ]
--- MikrotikManager/mikrotik/views.py
+++ MikrotikManager/mikrotik/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from .models import Mikrotik
+import paramiko
 
 
 @login_required
@@ -18,10 +19,11 @@ def MikManger(request, mikrotik_name):
     if request.user.is_authenticated: # TODO: what happens if user isn't authenticated?
         user_id = request.user.id
     Router = Mikrotik.objects.filter(Userid = user_id, MikName=mikrotik_name)
-    Commands = ["\interface print", "\ip address print"]
+    router = Router[0].MikName
+    Commands = ["interface Print", "ipAddress Print"]
     if Router:
         context = {
-            "Mikrotik": Router,
+            "Mikrotik": router,
             "Commands": Commands
         }
         return render(request, "mikrotik/manager.html", context=context)
@@ -30,3 +32,25 @@ def MikManger(request, mikrotik_name):
             "error": "The selected router does not exist?!\nIDK! go ask the admin!"
         }
         return render(request, "mikotik/index.html", context=error)
+
+@login_required
+def MikCommand(request, mikrotik_name, Routercommand):
+    if request.user.is_authenticated: # TODO: what happens if user isn't authenticated?
+        user_id = request.user.id
+    Router = Mikrotik.objects.filter(Userid = user_id, MikName=mikrotik_name)
+    router = Router[0]
+
+    ssh = paramiko.SSHClient()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy)
+    ssh.connect(hostname=router.MikIp, port=router.MikPort,
+                username=router.MikUsername, password=router.MikPassword, look_for_keys=False)
+    command = "\\" + Routercommand
+    print(command)
+    stdin, stdout, stderr = ssh.exec_command(command)
+    res = stdout.read().decode()
+    print(res)
+    ssh.close()
+    result = {
+        "result": res
+    }
+    return render(request, "mikrotik/manager.html", context=result)
